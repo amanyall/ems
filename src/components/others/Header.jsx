d0adfafcc5847e177549765918ebff3fdfcaff64
amanyall <amanpandey5961@gmail.com>
2024-11-08 11:52:26 +0530

    Initial commit

---
+++ src/components/others/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex items-end justify-between'>
+      <h1 className='text-2xl font-medium'>
+        Hello 
+        <br />
+        <span className='text-3xl font-semibold'> Aman </span>
+        </h1>
+      <button className='bg-red-600 text-lg font-medium text-white px-5 py-2 rounded-sm'>Log out</button>
+    </div>
+  )
+}
+
+export default Header
