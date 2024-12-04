0a2099f9ddd94c116f565f3644dbfa519d7c27f8
amanyall <amanpandey5961@gmail.com>
2024-12-04 23:52:48 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
+import CreateTask from './components/others/CreateTask'
 
 const App = () => {
   return (
     <>
     {/* <Login/> */}
     {/* <EmployeeDashboard/> */}
-    <AdminDashboard/>
+    {/* <AdminDashboard/> */}
+    <CreateTask/>
     </>
   )
 }
